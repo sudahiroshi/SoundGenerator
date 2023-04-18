e11f6ace8a70bd30878f9f9713eeae6d0a9a8470
Hiroshi Suda <sudahiroshi@gmail.com>
2023-04-18 21:07:18 +0900

    和音に対応

--- sound.js
+++ sound.js
@@ -21,21 +21,38 @@ class Sound {
      * 基本波形を鳴らす
      * @param {String} type 波形{"sine", "square", "triangle", "sawtooth"のいずれか}
      * @param {Number} gain 音量
-     * @param {Object} param 周波数と鳴らす時間のハッシュ
+     * @param {Object} param 周波数の配列と鳴らす時間のハッシュ
      */
     play( type, gain, param ) {
-        let frequency = param.freq || 440;
+        let frequency = param.freq || [440];
         let duration = param.duration || 1000;
-        let oscNode = this.ctx.createOscillator();
-        let gainNode = this.ctx.createGain();
+        let oscNode = new Array( frequency.length );
+        let gainNode = new Array( frequency.length );
 
-        oscNode.connect( gainNode );
-        gainNode.connect( this.ctx.destination );
-        oscNode.type = type;
-        oscNode.frequency.value = frequency;
-        gainNode.gain.value = gain;
-        oscNode.start();
-        setTimeout( () => { oscNode.stop() }, duration );
+        for( let i=0; i<oscNode.length; i++ ) {
+            oscNode[i] = this.ctx.createOscillator();
+            gainNode[i] = this.ctx.createGain();
+
+            oscNode[i].connect( gainNode[i] );
+            gainNode[i].connect( this.ctx.destination );
+
+            oscNode[i].type = type;
+            oscNode[i].frequency.value = frequency[i];
+            gainNode[i].gain.value = gain;
+
+            let dummy = this.ctx.currentTime + duration / 1000.0;
+            oscNode[i].start();
+            oscNode[i].stop( dummy );
+        }
+        // this.ctx.createGain();
+
+        // oscNode.connect( gainNode );
+        // gainNode.connect( this.ctx.destination );
+        // oscNode.type = type;
+        // oscNode.frequency.value = frequency;
+        // gainNode.gain.value = gain;
+        // oscNode.start();
+        //setTimeout( () => { oscNode.stop() }, duration );
     }
 
     /**
@@ -64,60 +81,54 @@ class Sound {
     /**
      * バイオリン：高い周波数成分まで含まれる音
      * @param {Number} gain 音量
-     * @param {Object} param 周波数と鳴らす時間のハッシュ
+     * @param {Object} param 周波数の配列と鳴らす時間のハッシュ
      */
     violin( gain, param ) {
-        let frequency = param.freq || 440;
+        let frequency = param.freq || [440];
         let duration = param.duration || 1000;
-        let [oscNode, gainNode] = this.inst( this.number, frequency, gain );
-        for( let i=1; i<this.number; i++ ) {
-            oscNode[i].start();
-        }
-        setTimeout( () => {
+        for( let j=0; j<frequency.length; j++ ) {
+            let [oscNode, gainNode] = this.inst( this.number, frequency[j], gain );
             for( let i=1; i<this.number; i++ ) {
-                oscNode[i].stop();
+                oscNode[i].start();
+                oscNode[i].stop( this.ctx.currentTime + duration / 1000.0 );
             }
-        }, duration );
+        }
     }
 
     /**
      * クッリネット：奇数倍音で構成される音
      * @param {Number} gain 音量
-     * @param {Object} param 周波数と鳴らす時間のハッシュ
+     * @param {Object} param 周波数の配列と鳴らす時間のハッシュ
      */
     clarinet( gain, param ) {
-        let frequency = param.freq || 440;
+        let frequency = param.freq || [440];
         let duration = param.duration || 1000;
-        let [oscNode, gainNode] = this.inst( this.number, frequency, gain );
-        oscNode[1].start();
-        for( let i=2; i<this.number; i+=2 ) {
-            oscNode[i].start();
-        }
-        setTimeout( () => {
-            oscNode[1].stop();
+        for( let j=0; j<frequency.length; j++ ) {
+            let [oscNode, gainNode] = this.inst( this.number, frequency[j], gain );
+            oscNode[1].start();
+            oscNode[1].stop( this.ctx.currentTime + duration / 1000.0 );
             for( let i=2; i<this.number; i+=2 ) {
-                oscNode[i].stop();
+                oscNode[i].start();
+                oscNode[i].stop( this.ctx.currentTime + duration / 1000.0 );
             }
-        }, duration );
+        }
     }
 
     /**
      * ピアノ：偶数倍音で構成される音
      * @param {Number} gain 音量
-     * @param {Object} param 周波数と鳴らす時間のハッシュ
+     * @param {Object} param 周波数の配列と鳴らす時間のハッシュ
      */
     piano( gain, param ) {
-        let frequency = param.freq || 440;
+        let frequency = param.freq || [440];
         let duration = param.duration || 1000;
-        let [oscNode, amp] = this.inst( this.number, frequency, gain );
-        for( let i=1; i<this.number; i+=2 ) {
-            oscNode[i].start();
-        }
-        setTimeout( () => {
+        for( let j=0; j<frequency.length; j++ ) {
+            let [oscNode, amp] = this.inst( this.number, frequency[j], gain );
             for( let i=1; i<this.number; i+=2 ) {
-                oscNode[i].stop();
+                oscNode[i].start();
+                oscNode[i].stop( this.ctx.currentTime + duration / 1000.0 );
             }
-        }, duration );
+        }
     }
 
     /**
@@ -126,23 +137,26 @@ class Sound {
      * @param {Object} param 周波数と鳴らす時間のハッシュ
      */
     flute( gain, param ) {
-        let frequency = param.freq || 440;
+        let frequency = param.freq || [440];
         let duration = param.duration || 1000;
-        let [oscNode, amp] = this.inst( this.number, frequency, gain );
-        for( let i=1; i<6; i++ ) {
-            oscNode[i].start();
-        }
-        setTimeout( () => {
+        for( let j=0; j<frequency.length; j++ ) {
+            let [oscNode, amp] = this.inst( this.number, frequency[j], gain );
             for( let i=1; i<6; i++ ) {
-                oscNode[i].stop();
+                oscNode[i].start();
+                oscNode[i].stop( this.ctx.currentTime + duration / 1000.0 );
             }
-        }, duration );
+        }
     }
 }
 
 function getParam() {
-    let freq = document.querySelector('#freq').value;
-    let duration = document.querySelector('#bar').value;
+    let freq1 = Number( document.querySelector('#freq1').value );
+    let freq2 = Number( document.querySelector('#freq2').value );
+    let freq3 = Number( document.querySelector('#freq3').value );
+    let duration = Number( document.querySelector('#bar').value );
+    freq = [ freq1 ];
+    if( freq2 != 0 ) freq.push( freq2 );
+    if( freq3 != 0 ) freq.push( freq3 );
     console.log( freq );
-    return { freq: Number(freq), duration: Number(duration) };
+    return { freq: freq, duration: duration };
 }
